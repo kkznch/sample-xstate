bcff19df4c37cddcd11f2328473179f84ddb9919
kekezun <kkznch710md@gmail.com>
2024-03-20 08:40:17 +0900

    pnpm @xstate/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import type { FC } from 'react';
+import { useMachine } from '@xstate/react';
+import { createMachine } from 'xstate';
+
+const toggleMachine = createMachine({
+  id: 'toggle',
+  initial: 'Inactive',
+  states: {
+    Inactive: {
+      on: { toggle: 'Active' },
+    },
+    Active: {
+      on: { toggle: 'Inactive' },
+    },
+  },
+});
 
 const App: FC = () => {
-  return <h1>Hello, world!</h1>;
+  const [state, send] = useMachine(toggleMachine);
+
+  return (
+    <div>
+      <div>Value: {state.value.toString()}</div>
+      <button onClick={() => send({ type: 'toggle' })}>Toggle</button>
+    </div>
+  );
 };
 
 export default App;
